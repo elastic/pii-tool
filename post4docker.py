1581ff95fb1054b679163f773a3ca66e6f3c686d
untergeek <aaron@mildensteins.com>
2024-10-04 12:42:09 -0600

    Cosmetic changes
    Explain what's going on in improved comments

--- post4docker.py
+++ post4docker.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Post Docker 'build' phase script"""
+"""Post Docker 'build' script"""
 import shutil
 from platform import machine, system, python_version
 
 MAJOR, MINOR = tuple(python_version().split('.')[:-1])
 SYSTEM = system().lower()
-BUILD = f'build/exe.{system().lower()}-{machine()}-{MAJOR}.{MINOR}'
+# cx_Freeze builds go into a sophisticated subpath based on these values:
+BUILD = f'build/exe.{SYSTEM}-{machine()}-{MAJOR}.{MINOR}'
 TARGET = 'executable_build'
 
-# Rename the path of BUILD to be generic enough for Dockerfile to get
+# Rename the path of BUILD to be generic enough (TARGET) for Dockerfile to copy
 shutil.move(BUILD, TARGET)
